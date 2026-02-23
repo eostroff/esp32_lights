82b228e9cd8080e8342917106e22d57abba151f5
Ethan Ostroff <ethanostroff1@gmail.com>
2026-02-23 12:05:19 -0500

    connect to wifi and blink led/print for status

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include "wifi_info.h"
+
+#define LED_PIN 2
 
 // put function declarations here:
-int myFunction(int, int);
+void blinkLed();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected to Wi-Fi!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Gateway (Router IP): ");
+  Serial.println(WiFi.gatewayIP()); // Get router's IP address
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  blinkLed();
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void blinkLed() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
 }
